2089011684f57d512f2d69366f4f56dc9db28355
Andres Gutierrez <agfeynman@gmail.com>
2022-01-19 00:55:01 +0000

    refactor: abstract server creation

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const createApp = require('./app');
 
 const port = process.env.PORT;
 
-const config = {
-  connectionString: process.env.PG_URI,
-};
-
-createApp(config).then((app) => {
+createApp().then((app) => {
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
   });
